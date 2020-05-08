de1f9fa4d577f118338c1dea4473a18ab3027d67
Stefan <stefansdjindjic@gmail.com>
2020-05-08 09:53:56 +0200

    part 1

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Car;
+
+class CarController extends Controller
+{
+     /**
+     * Get Car
+     *
+     * @return Response
+     */
+    public function index(string $id)
+    {
+        return 'xaxaxa';
+    }
+}
---
+++ app/Http/Models/Car.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    private $id;
+    private $uid;
+    private $detailUrl;
+    private $importId;
+    private $hasChanges;
+    private $car;
+    private $yearOfProduction;
+    private $titleShort;
+    private $horsepower;
+    private $cubicCapacity;
+    private $manufacturerCode;
+    private $steering;
+    private $cylinder;
+    private $gearbox;
+    private $gears;
+    private $gearType;
+    private $frontBrakes;
+    private $rearBrakes;
+    private $doors;
+    private $fuel;
+    private $firstRegistrationDate;
+    private $numberOfPreviousOwner;
+    private $chassisNumber;
+    private $engineNumber;
+    private $gearboxNumber;
+    private $matchingNumbers;
+    private $replica;
+    private $converted;
+    private $colorOutside;
+    private $colorInside;
+    private $manufacturerColorNameOutside;
+    private $manufacturerColorNameInside;
+    private $interiorMaterial;
+    private $sunroof;
+    private $electricWindows;
+    private $foldingRoof;
+    private $centralLocking;
+    private $airbag;
+    private $heatedSeats;
+    private $abs;
+    private $airconditioning;
+    private $powerSteering;
+    private $cruiseControl;
+    private $price;
+    private $priceCurrency;
+    private $priceOnRequest;
+    private $mileageUnit;
+    private $mileageByUnit;
+    private $vatReclaimable;
+    private $location;
+    private $standardImages;
+    private $damageImages;
+    private $restorationImages;
+    private $embeddedVideoCode;
+    private $description;
+    private $stateCategory;
+    private $extras;
+    private $report;
+    private $starsEngine;
+    private $starsTechnik;
+    private $starsPaint;
+    private $starsInterior;
+    private $licensed;
+    private $readyToRide;
+    private $milleMiglia;
+    private $mainInspection;
+    private $oldtimerLicensePlate;
+    private $fiva;
+    private $fia;
+    private $accidentFree;
+    private $mainInspectionUntil;
+    private $files;
+    private $contact;
+    private $status;
+    private $locale;
+    private $datePublished;
+    private $updatedAt;
+
+}
